00b5318f28dfa1c6cce85c7f950e8cc8004cca77
Jordan Nourry <jordan.nourry@gmail.com>
2019-03-07 22:34:18 +0100

    Refactoring test

--- src/test/kotlin/org/craftsrecords/talkadvisorconsumer/contracts/TalkAdvisorCheckTest.kt
+++ src/test/kotlin/org/craftsrecords/talkadvisorconsumer/contracts/TalkAdvisorCheckTest.kt
@@ -6,13 +6,11 @@ import org.craftsrecords.talkadvisorconsumer.infra.resources.preferences.Topic
 import org.craftsrecords.talkadvisorconsumer.infra.resources.talk.TalkFormat
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner
 import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureStubRunner(workOffline = true, ids = ["org.craftsrecords:talkadvisor-infra-application:+:8090"])
 class TalkAdvisorCheckTest {
 
